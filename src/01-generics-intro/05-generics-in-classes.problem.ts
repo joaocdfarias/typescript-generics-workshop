ef0a856cf045af2b7c26243fe17ec15188df5cfe
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-12-12 20:51:15 -0300

    feat: generic classes

---
+++ src/01-generics-intro/05-generics-in-classes.problem.ts
@@ -0,0 +1,24 @@
+import { expect } from "jsr:@std/expect/expect";
+import { Equal, Expect } from "../helpers/type-utils.ts";
+
+export class Component {
+  private props: unknown;
+
+  constructor(props: unknown) {
+    this.props = props;
+  }
+
+  getProps = () => this.props;
+}
+
+Deno.test("Should create an object containing props", () => {
+  const component = new Component({ a: 1, b: 2, c: 3 });
+
+  const result = component.getProps();
+
+  expect(result).toEqual({ a: 1, b: 2, c: 3 });
+
+  type tests = [
+    Expect<Equal<typeof result, { a: number; b: number; c: number }>>
+  ];
+});
