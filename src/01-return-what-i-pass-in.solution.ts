dacc81ef8aec22bc672623164d402d75d28f929f
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-12-11 21:30:33 -0300

    feat: problem 1

---
+++ src/01-return-what-i-pass-in.solution.ts
@@ -0,0 +1,10 @@
+import { Expect, Equal } from "./helpers/type-utils.ts";
+
+const returnWhatIPassIn = <T>(t: T) => {
+  return t;
+};
+
+const one = returnWhatIPassIn(1); //Type: <1>(t: 1) => 1
+const matt = returnWhatIPassIn("matt"); //Type: <"matt">(t: "matt") => "matt"
+
+type tests = [Expect<Equal<typeof one, 1>>, Expect<Equal<typeof matt, "matt">>];
