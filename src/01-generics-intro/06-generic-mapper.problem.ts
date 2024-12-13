cda100d22c2261c5f77eafad5058befa547862a4
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-12-12 21:09:17 -0300

    feat: add object property constraints to a generic

---
+++ src/01-generics-intro/06-generic-mapper.problem.ts
@@ -0,0 +1,90 @@
+import { expect } from "jsr:@std/expect";
+import { Equal, Expect } from "../helpers/type-utils.ts";
+
+export const concatenateFirstNameAndLastName = (user: unknown) => {
+  return {
+    ...user,
+    fullName: `${user.firstName} ${user.lastName}`,
+  };
+};
+
+Deno.test(
+  "Should add fullName to an object which only contains firstName and lastName",
+  () => {
+    const users = [
+      {
+        firstName: "Matt",
+        lastName: "Pocock",
+      },
+    ];
+
+    const newUsers = users.map(concatenateFirstNameAndLastName);
+
+    expect(newUsers).toEqual([
+      {
+        firstName: "Matt",
+        lastName: "Pocock",
+        fullName: "Matt Pocock",
+      },
+    ]);
+
+    type tests = [
+      Expect<
+        Equal<
+          typeof newUsers,
+          Array<{ firstName: string; lastName: string } & { fullName: string }>
+        >
+      >
+    ];
+  }
+);
+
+Deno.test("Should retain other properties passed in", () => {
+  const users = [
+    {
+      id: 1,
+      firstName: "Matt",
+      lastName: "Pocock",
+    },
+  ];
+
+  const newUsers = users.map(concatenateFirstNameAndLastName);
+
+  expect(newUsers).toEqual([
+    {
+      id: 1,
+      firstName: "Matt",
+      lastName: "Pocock",
+      fullName: "Matt Pocock",
+    },
+  ]);
+
+  type tests = [
+    Expect<
+      Equal<
+        typeof newUsers,
+        Array<
+          { id: number; firstName: string; lastName: string } & {
+            fullName: string;
+          }
+        >
+      >
+    >
+  ];
+});
+
+Deno.test(
+  "Should fail when the object passed in does not contain firstName",
+  () => {
+    const users = [
+      {
+        firstName: "Matt",
+      },
+    ];
+
+    const newUsers = users.map(
+      // @ts-expect-error
+      concatenateFirstNameAndLastName
+    );
+  }
+);
