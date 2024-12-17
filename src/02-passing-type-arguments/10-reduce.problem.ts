a7cdfe8e6a64098ad2eeb7529c637844231963ec
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2024-12-17 19:53:01 -0300

    feat: infering reduce type

---
+++ src/02-passing-type-arguments/10-reduce.problem.ts
@@ -0,0 +1,29 @@
+import { expect } from "jsr:@std/expect";
+import { Equal, Expect } from "../helpers/type-utils.ts";
+
+const array = [
+  {
+    name: "John",
+  },
+  {
+    name: "Steve",
+  },
+];
+
+const obj = array.reduce((accum, item) => {
+  accum[item.name] = item;
+  return accum;
+}, {});
+
+Deno.test("Should resolve to an object where name is the key", () => {
+  expect(obj).toEqual({
+    John: {
+      name: "John",
+    },
+    Steve: {
+      name: "Steve",
+    },
+  });
+
+  type tests = [Expect<Equal<typeof obj, Record<string, { name: string }>>>];
+});
